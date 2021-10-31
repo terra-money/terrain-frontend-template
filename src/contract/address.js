9fb568ebfbaa7f4ecf3b4f3ff67307982f4bfe5d
Supanat Potiwarakorn <supanat.ptk@gmail.com>
2021-10-31 12:52:24 +0700

    change refs location

--- src/contract/address.js
+++ src/contract/address.js
@@ -1,2 +1,3 @@
-import config from "../config.terrain.json"
+// sync-ed from root via `tr sync-refs`
+import config from "../refs.terrain.json"
 export const contractAdress = (wallet) => config[wallet.network.name].counter.contractAddresses.default
